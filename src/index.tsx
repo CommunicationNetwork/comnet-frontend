6d309b49521569b18783050c999b6400f5fd0d14
myyxl <22593897+myyxl@users.noreply.github.com>
2022-04-18 15:45:58 +0200

    feat(Font): load font from own server

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
+import './css/fonts/fonts.css'
 import CustomThemeProvider from './components/theme/CustomThemeProvider';
 import Dashboard from 'components/Dashboard';
 
